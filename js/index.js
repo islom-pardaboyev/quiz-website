a6e222481e4e3ede484aec99049ed0ec65011fab
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-03-15 09:12:32 +0500

    Updated File

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 const startBtn = document.querySelector(".start-btn");
 const popupInfo = document.querySelector(".popup-info");
+const exitBtn = document.querySelector(".exit-btn");
+console.log(exitBtn);
 
 startBtn.onclick = () => {
     popupInfo.classList.add("active")
+}
+
+
+exitBtn.onclick = () => {
+    popupInfo.classList.remove("active")
 }
